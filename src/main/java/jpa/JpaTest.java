92ca181bf835a2d0652280869769b8a08d2bd960
LeaderKam <you@example.com>
2020-04-16 00:49:08 +0200

    mysql resolve

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,21 +1,28 @@
 package jpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 
-import jpa.entities.Reunion;
-import jpa.entities.User;
 import test.testjpa.domain.Department;
+import test.testjpa.domain.Employee;
+
+import java.util.List;
 
 public class JpaTest {
+
+	private EntityManager manager;
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		EntityManager manager = EntityManagerHelper.getEntityManager();
+		EntityManagerFactory factory =
+				Persistence.createEntityManagerFactory("mysql");
+		//EntityManager manager = EntityManagerHelper.getEntityManager();
+		EntityManager manager = factory.createEntityManager();
+		JpaTest test = new JpaTest(manager);
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 		
@@ -29,17 +36,38 @@ public class JpaTest {
 //		
 
 		try {
-
+			test.createEmployees();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
 
-
+//test.listEmployees();
 		manager.close();
+		System.out.println(".. done");
 		EntityManagerHelper.closeEntityManagerFactory();
 		//		factory.close();
 	}
+
+	private void createEmployees() {
+		int numOfEmployees = manager.createQuery("Select a From Employee a",
+				Employee.class).getResultList().size();
+		if (numOfEmployees == 0) {
+			Department department = new Department("java");
+			manager.persist(department);
+			manager.persist(new Employee("Jakab Gipsz",department));
+			manager.persist(new Employee("Captain Nemo",department));
+		}
+	}
+	private void listEmployees() {
+		List<Employee> resultList = manager.createQuery("Select a From Employee a",
+				Employee.class).getResultList();
+		System.out.println("num of employess:" + resultList.size());
+		for (Employee next : resultList) {
+			System.out.println("next employee: " + next);
+		}
+	}
+
 
 
 }
--- src/main/java/jpa/reporitories/UserRepository.java
+++ src/main/java/jpa/reporitories/UserRepository.java
@@ -1,7 +1,5 @@
 package jpa.reporitories;
 
-import jpa.entities.User;
-
 public interface UserRepository /*extends JpaRepository<User,Integer>*/ {
 
 }
